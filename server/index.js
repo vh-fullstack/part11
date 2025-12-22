76b2d747b0c7d8d9ed1333e1176f37246f6a825a
vh-fullstack <109882814+vh-fullstack@users.noreply.github.com>
2025-12-22 11:49:26 -0500

    fix: correct socket path

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ const start = async () => {
 
   const wsServer = new WebSocketServer({
     server: httpServer,
-    path: '/',
+    path: '/graphql',
   })
 
   const serverCleanup = useServer({ schema }, wsServer)
